b828e06e244826659c26eb5ae21d856101e1dad3
Ankit Kaushal <kaushalankit101010@gmail.com>
2024-04-20 17:14:00 +0530

    😵‍💫 WIP: update: added intervals

--- contracts/facets/DCAFacet.sol
+++ contracts/facets/DCAFacet.sol
@@ -2,54 +2,95 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../interfaces/IDCA.sol";
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../interfaces/IDCA.sol"; // ! TODO: later add this interface.
+import "hardhat/console.sol";
 
-contract DCAFacet is IDCA, ReentrancyGuard {
+interface IAggregatorV3 {
+    function getLatestPrice(address token) external view returns (int);
+}
+
+contract DCAFacet is ReentrancyGuard {
+    address private _priceAggregator;
+
     struct DCAOrder {
         address tokenAddress;
-        uint256 amount;
+        uint256 totalAmount;
         uint256 interval;
         uint256 nextExecutionTime;
+        uint256 orderCount;
+        uint256 ordersPlaced;
     }
+    event AllOrdersExecuted(address indexed user, uint256 index);
+    event DCASetup(
+        address indexed user,
+        address token,
+        uint256 amount,
+        uint256 interval,
+        uint256 orderCount
+    );
+    event DCAExecuted(address indexed user, address token, uint256 amount);
 
     mapping(address => DCAOrder[]) public userOrders;
 
     function setupDCA(
         address _tokenAddress,
-        uint256 _amount,
-        uint256 _interval
-    ) external override {
+        uint256 _totalAmount,
+        uint256 _interval,
+        uint256 _orderCount
+    ) external {
         require(_interval > 0, "Interval must be > 0");
-        console.log("C:: adding");
+        require(_orderCount > 0, "Order count must be > 0");
+        require(_totalAmount >= _orderCount, "Total amount too low");
+        // uint256 amountPerOrder = _totalAmount / _orderCount;
+
         userOrders[msg.sender].push(
             DCAOrder({
                 tokenAddress: _tokenAddress,
-                amount: _amount,
+                totalAmount: _totalAmount,
                 interval: _interval,
-                nextExecutionTime: block.timestamp + _interval
+                nextExecutionTime: block.timestamp + _interval,
+                orderCount: _orderCount,
+                ordersPlaced: 0
             })
         );
-        emit DCASetup(msg.sender, _tokenAddress, _amount, _interval);
+        emit DCASetup(
+            msg.sender,
+            _tokenAddress,
+            _totalAmount,
+            _interval,
+            _orderCount
+        );
     }
 
-    function executeDCA(uint index) public override {
+    function executeDCA(uint index) public {
         DCAOrder storage order = userOrders[msg.sender][index];
         require(
             block.timestamp >= order.nextExecutionTime,
             "Execution too early"
         );
+        require(order.ordersPlaced < order.orderCount, "All orders executed");
+
+        uint256 amountPerOrder = order.totalAmount / order.orderCount;
+
+        // Transfer partial token
         require(
             IERC20(order.tokenAddress).transferFrom(
                 msg.sender,
                 address(this),
-                order.amount
+                amountPerOrder
             ),
             "Transfer failed"
         );
+        order.ordersPlaced++;
         order.nextExecutionTime += order.interval;
-        emit DCAExecuted(msg.sender, order.tokenAddress, order.amount);
+        emit DCAExecuted(msg.sender, order.tokenAddress, amountPerOrder);
+
+        // !TODO: #pending check if all orders are placed and clean up or reset the order
+        if (order.ordersPlaced == order.orderCount) {
+            emit AllOrdersExecuted(msg.sender, index);
+            // ....
+        }
     }
 
     function checkUpkeep(
