82419ea672c77774d73e43963f964ceaef27d218
Ankit Kaushal <kaushalankit101010@gmail.com>
2024-04-20 17:15:04 +0530

    Update IDCA.sol

--- contracts/interfaces/IDCA.sol
+++ contracts/interfaces/IDCA.sol
@@ -6,14 +6,16 @@ interface IDCA {
         address indexed user,
         address token,
         uint256 amount,
-        uint256 interval
+        uint256 interval,
+        uint256 orderCount
     );
     event DCAExecuted(address indexed user, address token, uint256 amount);
 
     function setupDCA(
         address _tokenAddress,
         uint256 _amount,
-        uint256 _interval
+        uint256 _interval,
+        uint256 _orderCount
     ) external;
 
     function executeDCA(uint index) external;
