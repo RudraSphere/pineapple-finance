49d67465db0dbe9573acabfebabc6a99a2ad1435
Ankit Kaushal <kaushalankit101010@gmail.com>
2024-04-29 02:41:34 +0530

    📦 Update: Packages + Tailwind

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -13,8 +13,98 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        shimmer: {
+          "0%": {
+            transform: "translateX(-100%) rotate(30deg)",
+          },
+          "100%": {
+            transform: "translateX(200%) rotate(30deg)",
+          },
+        },
+        wiggle: {
+          "0%, 100%": {
+            transform: "rotate(-3deg)",
+          },
+          "50%": {
+            transform: "rotate(3deg)",
+          },
+        },
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        "fade-in-left": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(10px)",
+            visibility: "hidden",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+            visibility: "visible",
+          },
+        },
+        "fade-out-left": {
+          "0%": {
+            opacity: "1",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateX(10px)",
+            visibility: "hidden",
+          },
+        },
+        "fade-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(10px)",
+          },
+        },
+      },
+      animation: {
+        shimmer: "shimmer 2.5s ease-in-out infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
+        "fade-in-left": "fade-in-left 0.5s ease-out",
+        "fade-out-left": "fade-out-left 0.5s ease-out",
+        "fade-in-right": "fade-in-left 0.5s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar-hide")],
 };
 export default config;
