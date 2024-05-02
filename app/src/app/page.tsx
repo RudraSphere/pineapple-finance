58ecfb82f3b28f083f6a8354482163f14ee33e29
Ankit Kaushal <kaushalankit101010@gmail.com>
2024-05-02 07:05:18 +0530

    Update: UI

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,24 @@
 'use client'
 
+import Link from 'next/link'
 import SwapForm from './components/SwapForm'
 
 export default function Home() {
   return (
     <>
+      <div className='mx-auto my-4 max-w-4xl text-center'>
+        <h4 className='font-heading text-lg font-semibold'>
+          Personal Project Deployed on a Forked Version of the Polygon Network on AWS EC2 (Internal
+          RPC).
+        </h4>
+        <p className='font-sans'>
+          Get free Tokens from{' '}
+          <Link className='text-xl underline duration-200 hover:text-teal-600' href='/faucet'>
+            here.
+          </Link>
+        </p>
+      </div>
+
       <SwapForm />
     </>
   )
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,110 +1,109 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
         shimmer: {
-          "0%": {
-            transform: "translateX(-100%) rotate(30deg)",
+          '0%': {
+            transform: 'translateX(-100%) rotate(30deg)',
           },
-          "100%": {
-            transform: "translateX(200%) rotate(30deg)",
+          '100%': {
+            transform: 'translateX(200%) rotate(30deg)',
           },
         },
         wiggle: {
-          "0%, 100%": {
-            transform: "rotate(-3deg)",
+          '0%, 100%': {
+            transform: 'rotate(-3deg)',
           },
-          "50%": {
-            transform: "rotate(3deg)",
-          },
-        },
-        "fade-in-down": {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(-10px)",
-          },
-          "100%": {
-            opacity: "1",
-            transform: "translateY(0)",
+          '50%': {
+            transform: 'rotate(3deg)',
           },
         },
-        "fade-in-up": {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(10px)",
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
           },
-          "100%": {
-            opacity: "1",
-            transform: "translateY(0)",
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
           },
         },
-        "fade-out-up": {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-out-up': {
           from: {
-            opacity: "1",
-            transform: "translateY(0px)",
+            opacity: '1',
+            transform: 'translateY(0px)',
           },
           to: {
-            opacity: "0",
-            transform: "translateY(10px)",
+            opacity: '0',
+            transform: 'translateY(10px)',
           },
         },
-        "fade-in-left": {
-          "0%": {
-            opacity: "0",
-            transform: "translateX(10px)",
-            visibility: "hidden",
+        'fade-in-left': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(10px)',
+            visibility: 'hidden',
           },
-          "100%": {
-            opacity: "1",
-            transform: "translateX(0px)",
-            visibility: "visible",
-          },
-        },
-        "fade-out-left": {
-          "0%": {
-            opacity: "1",
-          },
-          "100%": {
-            opacity: "0",
-            transform: "translateX(10px)",
-            visibility: "hidden",
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0px)',
+            visibility: 'visible',
           },
         },
-        "fade-in-right": {
-          "0%": {
-            opacity: "0",
-            transform: "translateX(-10px)",
+        'fade-out-left': {
+          '0%': {
+            opacity: '1',
           },
-          "100%": {
-            opacity: "1",
-            transform: "translateX(10px)",
+          '100%': {
+            opacity: '0',
+            transform: 'translateX(10px)',
+            visibility: 'hidden',
+          },
+        },
+        'fade-in-right': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(10px)',
           },
         },
       },
       animation: {
-        shimmer: "shimmer 2.5s ease-in-out infinite",
-        wiggle: "wiggle 1s ease-in-out infinite",
-        "fade-in-down": "fade-in-down 0.5s ease-out",
-        "fade-in-up": "fade-in-up 0.5s ease-out",
-        "fade-out-up": "fade-out-up 0.5s ease-out",
-        "fade-in-left": "fade-in-left 0.5s ease-out",
-        "fade-out-left": "fade-out-left 0.5s ease-out",
-        "fade-in-right": "fade-in-left 0.5s ease-out",
+        shimmer: 'shimmer 2.5s ease-in-out infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-in-up': 'fade-in-up 0.5s ease-out',
+        'fade-out-up': 'fade-out-up 0.5s ease-out',
+        'fade-in-left': 'fade-in-left 0.5s ease-out',
+        'fade-out-left': 'fade-out-left 0.5s ease-out',
+        'fade-in-right': 'fade-in-left 0.5s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar-hide")],
-};
-export default config;
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
+}
+export default config
